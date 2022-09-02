81d3943c80d71aae0eb40ebbe13260a36318330c
Yuriy Tikhov <ahojte@gmail.com>
2022-09-03 02:42:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Web app with user/password authentication
+
+### How to run
+1. Install JDK 11 https://adoptium.net/temurin
+1. Install https://maven.apache.org/
+2. Run command:
+```
+mvn clean package
+```
+3. Run command:
+```
+java -jar -Dapp.user=<your user> -Dapp.password=<your password> target/auth.jar
+```
+for example
+```
+java -jar -Dapp.user=john -Dapp.password=MySecretPassword123 target/auth.jar
+```
+4. Open the page http://localhost:8080
---
+++ src/main/java/com/ytikhov/auth/config/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.ytikhov.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Value("${app.user}")
+    private String user;
+
+    @Value("${app.password}")
+    private String password;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username(this.user)
+                        .password(this.password)
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
